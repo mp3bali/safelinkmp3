402aa2aa36e72e349c067b7ab84c93f52cb9cebc
mp3bali <doedabali@gmail.com>
2017-06-28 13:56:56 +0800

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -22,7 +22,7 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="asuransi-kesehatan_26.html"}
-if(x=="2"){xxx="credit-card-dan-pengertiannya.html"}
+if(x=="2"){xxx="bni-scueritites-26.html"}
 if(x=="3"){xxx="bni-scuerities.html"}
 if(x=="4"){xxx="credit-card-dan-pengertiannya.html"}a_to_ve[i].href="https://safelink-mp3bali.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
