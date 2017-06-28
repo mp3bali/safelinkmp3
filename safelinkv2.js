e74be7770ef86c061ec0664f0829785cb7a7f57b
mp3bali <doedabali@gmail.com>
2017-06-28 14:28:26 +0800

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -19,12 +19,10 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*4)+ 1);
+var x=Math.floor((Math.random()*1)+ 1);
 var xxx=null;
 if(x=="1"){xxx="asuransi-kesehatan_26.html"}
-if(x=="2"){xxx="bni-scueritites-26.html"}
-if(x=="3"){xxx="bni-scuerities.html"}
-if(x=="4"){xxx="credit-card-dan-pengertiannya.html"}a_to_ve[i].href="https://safelink-mp3bali.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="https://safelink-mp3bali.blogspot.com/2017/06/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
